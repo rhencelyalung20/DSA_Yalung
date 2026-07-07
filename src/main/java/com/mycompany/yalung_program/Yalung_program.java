6eb6aca099089fbdfb86e066650d982baa0c5815
CL2-PC <CL2-PC@CL2-PC27>
2026-07-07 08:56:55 +0800

    ACT 2A First Program Repository

---
+++ src/main/java/com/mycompany/yalung_program/Yalung_program.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.yalung_program;
+import java.util.Scanner;
+
+/**
+ *
+ * @author CL2-PC
+ */
+public class Yalung_program {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello World!");
+        System.out.println("Yalung Rhencel John Agustin");
+        System.out.println("Enter a number: ");
+        int x = input.nextInt();
+        System.out.println("Enter a number: ");
+        int y = input.nextInt();
+        int sum;
+        sum = x + y;
+        System.out.println("Sum is: " + sum);
+    }
+}
